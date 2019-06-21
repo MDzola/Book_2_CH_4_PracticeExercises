825332199d6edf684e0431067725298243c10fc2
Michael Drew Palazola <mpalazola@hotmail.com>
2019-06-21 10:27:50 -0500

    uploading filed to github

---
+++ random.js
@@ -0,0 +1,17 @@
+function sayHelloName() {
+    return "Hello, Fred"
+}
+
+let greeting = sayHelloName();
+
+
+function createMessage() {
+    return `When I see my friend, I say ${sayHelloName()}`
+}
+
+function sayHello(name) {
+    return `hello, ${name}`
+}
+
+sayHello("John")
+
